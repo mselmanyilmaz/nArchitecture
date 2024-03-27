735f0074ff49b1483365457fab76107030664049
msyilmaz <mselman.yilmaz@spk.gov.tr>
2024-03-28 01:59:05 +0300

    Proje repoya manual olarak dahil edildi.

---
+++ src/demoProjects/carRentalApp/Domain/Entities/Brand.cs
@@ -0,0 +1,24 @@
+﻿using Core.Persistence.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Brand : Entity
+    {
+        public string Name { get; set; }
+
+        public Brand()
+        {
+        }
+
+        public Brand(int id, string name) : this() // Parametreli kurucu çalışırken parametresiz kurucunun da çalışması için this() kullanıldı.
+        {
+            Id = id;
+            Name = name;
+        }
+    }
+}
